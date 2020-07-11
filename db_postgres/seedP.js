67f7159076da05cb745245737c51a4c42a9c1e34
Jenine Steinberg <jenine.steinberg@gmail.com>
2020-07-10 23:22:11 -0700

    postgres seeding script

--- db_postgres/seedP.js
+++ db_postgres/seedP.js
@@ -5,7 +5,8 @@ const faker = require('faker');
 const writer = csvWriter();
 
 writer.pipe(fs.createWriteStream('./listings.csv'));
-for (var i = 0; i < 10000000; i += 1) {
+//change to correct size for actual seeding!!!
+for (var i = 0; i < 1000; i += 1) {
   writer.write({
     nightly_fee = nightly_fee: Math.ceil(Math.random() * 300) + 60,
     cleaning_fee: Math.ceil(Math.random() * 60) + 20,
@@ -14,6 +15,16 @@ for (var i = 0; i < 10000000; i += 1) {
     reviews: (Math.floor(Math.random() * 1000)),
     city: faker.address.city(),
     max_capacity: Math.ceil(Math.random() * 9) + 1
+  });
+}
+
+writer.pipe(fs.createWriteStream('./listings.csv'));
+//change to correct size for actual seeding!!!
+for (var i = 0; i < 5; i += 1) {
+  writer.write({
+    checkin: faker.date.between('2020-08-08', '2020-08-10'),
+    checkout: faker.date.between('2020-08-10', '2020-08-15'),
+    adults: Math.floor(Math.random() * 3) + 1
   });
 }
 
